cf003f2533fe81c80882119f718eb3a399309859
brmandw <amndofc@gmail.com>
2025-10-26 19:35:21 +0900

    Revert "Merge pull request #8 from Team-Susi-Pudjiastuti/rww-testing"
    This reverts commit 76433aca48b487e4a4142cca7be457c3bbabbab5, reversing
    changes made to 8f8fe6cae932b1780e6316ced38ca42566b4eb71.

--- routes/level.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const levelController = require('../controllers/level.controller');
-
-router.post('/', levelController.createLevel);
-
-module.exports = router;
--- routes/phase.router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const phaseController = require('../controllers/phase.controller');
-
-router.post('/', phaseController.createPhase);
-
-module.exports = router;
