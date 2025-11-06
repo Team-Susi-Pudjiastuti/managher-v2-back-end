9fe0df720013d2634f8f58421a0cf73871990547
brmandw <amndofc@gmail.com>
2025-11-06 21:28:19 +0700

    refactor(cors): simplify cors configuration by removing hardcoded origins

--- app.js
+++ app.js
@@ -6,17 +6,7 @@ const allRoutes = require('./routes/index');
 const port = process.env.PORT || 3000;
 const connectDB = require('./config/db');
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",      // frontend lokal (Next.js)
-      "https://managher-2.vercel.app" // nanti kalau frontend sudah di-deploy
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 connectDB();
 
