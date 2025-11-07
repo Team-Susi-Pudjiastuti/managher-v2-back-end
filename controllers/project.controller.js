16a05f3e1f2e05ea47b9a353b7e7b78f3667da6f
brmandw <amndofc@gmail.com>
2025-11-07 09:11:31 +0700

    fix(project.controller): enable RWWTesting creation and link to phase

--- controllers/project.controller.js
+++ controllers/project.controller.js
@@ -129,7 +129,7 @@ const createProjectPhase = async (projectId) => {
 
     const [businessIdea, rww, brandIdentity] = await Promise.all([
         BusinessIdea.create({ project: projectId }),
-        // RWWTesting.create({ project: projectId }),
+        RWWTesting.create({ project: projectId }),
         // ProductConcept.create({ project: projectId }),
         BrandIdentity.create({ project: projectId }),
     ]);
@@ -198,7 +198,10 @@ const createProjectPhase = async (projectId) => {
             icon: "CheckCircle",
             xp: 20,
             badge: "Pengambil Keputusan",
-            entities: [],
+            entities: [{
+                entity_type: 'rww_testing',
+                entity_ref: rww._id,
+            }],
             path: 'level_2_rww',
         },
         {
