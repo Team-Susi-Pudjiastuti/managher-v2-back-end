96ab6d9990957c47af026b53997500dcea2b1685
dwikarunia0001 <dwikarunia14157375@gmail.com>
2025-11-24 14:14:42 +0700

    fixing bug in cloudinary and controller level 3

--- routes/brandIdentity.router.js
+++ routes/brandIdentity.router.js
@@ -1,11 +1,12 @@
+// routes/brandIdentityRoutes.js
 const express = require('express');
 const brandIdentityController = require('../controllers/brandIdentity.controller');
 const router = express.Router();
 const upload = require('../utils/multer');
 
-// router.post('/', brandIdentityController.createBrandIdentity);
-router.get('/:project/', brandIdentityController.getBrandIdentity);
-router.put('/:id/', brandIdentityController.updateBrandIdentity);
+// ✅ HAPUS SLASH DI AKHIR SEMUA ROUTE
+router.get('/:project', brandIdentityController.getBrandIdentity);
+router.put('/:id', brandIdentityController.updateBrandIdentity);
 router.put('/:id/logoPreview', upload.single('file'), brandIdentityController.updateBrandIdentityLogoPreview);
 
 module.exports = router;
